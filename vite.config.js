97945a033db78375d656ecc2e1994eba8ccbbaae
admin <gzabun@hepsiemlak.com>
2022-12-10 00:08:06 +0300

    add rewrites

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,14 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  rewrites: [{ "source": "/(.*)", "destination": "/" }],
   base:'',
-  plugins: [vue()],
+  plugins: [vue(),pluginRewriteAll()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
