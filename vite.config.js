82fc001cf7abc0dd8ab4cc71590f740649ccc532
admin <gzabun@hepsiemlak.com>
2022-12-11 00:10:30 +0300

    vite config change base

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import pluginRewriteAll from 'vite-plugin-rewrite-all';
-
-
 
 export default defineConfig({
   base:'./',
-  plugins: [vue(),pluginRewriteAll()],
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,26 +12,6 @@ export default defineConfig({
   },
   server:{
     port: '5555'
-  },
-  build:{
-    rollupOptions: {
-      input:'index.html',
-      output: {
-        assetFileNames: (assetInfo) => {
-          var info = assetInfo.name.split(".");
-          var extType = info[info.length - 1];
-          if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType)) {
-            extType = "img";
-          } else if (/woff|woff2/.test(extType)) {
-            extType = "./";
-          }
-          return `${extType}/[name][extname]`;
-        },
-        chunkFileNames: "[name].js",
-        entryFileNames: "[name].js",
-      },
-    },
-    copyPublicDir:false,
   },
   css: {
     preprocessorOptions: {
